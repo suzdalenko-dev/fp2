1bbbd5bcebc310eff894bd4f91bc7949a457ee4a
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2024-12-04 16:26:24 +0100

    suzdalenko

--- DWES/Tutoria2_1eval/crear_usuario.sql
+++ DWES/Tutoria2_1eval/crear_usuario.sql
@@ -2,8 +2,8 @@
 use proyecto;
 -- 2.- Creamos la tabla usuarios
 create table usuarios(
-usuario varchar(20) primary key,
-pass varchar(64) not null
+    usuario varchar(20) primary key,
+    pass varchar(64) not null
 );
 -- 3.- Creamos un par de usuarios de prueba, vamos a utilizar sha256
 -- Para guardar las contraseñas, en realidad guardamos el hash.
--- Main.hs
+++
@@ -1,5 +0,0 @@
-main :: IO ()
-main = do
-    let x = 5
-    let result = if x > 3 then "Mayor que 3" else "Menor o igual a 3"
-    putStrLn result
